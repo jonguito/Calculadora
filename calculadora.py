79e096d58770aeec2453c0427084a1d25aad72da
jonga <jvfps20@gmail.com>
2024-08-20 16:41:14 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -13,26 +13,30 @@ while True:
         adiçao1 = float(input('Digite o primeiro valor que deseja fazer a adição:'))
         adiçao2 = float(input('Digite o segundo valor que deseja fazer a adição: '))
         result_1 = int(adiçao1 + adiçao2) 
-        print(f'''{adiçao1} + {adiçao2} = {result_1}
-===============================================================''')   
-    if operaçao == 2:
+        print(f'{adiçao1} + {adiçao2} = {result_1}')
+        print('+='*40)
+        
+    elif operaçao == 2:
         subtraçao1 = float(input('Digite o primeiro valor que deseja fazer a subtração: '))
         subtraçao2 = float(input('Digite o segundo valor que deseja fazer a subtração: '))
         result_2 = int(subtraçao1 - subtraçao2)
-        print(f'''{subtraçao1} - {subtraçao2} = {result_2}
-===============================================================''') 
-    if operaçao == 3:
+        print(f'{subtraçao1} - {subtraçao2} = {result_2}') 
+        print('+='*40)
+        
+    elif operaçao == 3:
         divisao1 = float(input('Digite o primeiro valor que deseja dividir: '))
         divisao2 = float(input('Digite o segundo valor que deseja dividir: '))
         result3 = int(divisao1 / divisao2)
-        print (f'''{divisao1} / {divisao2} = {result3}
-===============================================================''')   
-    if operaçao == 4:
+        print (f'{divisao1} / {divisao2} = {result3}') 
+        print('+='*40)
+        
+    elif operaçao == 4:
         multiplicaçao1 = float(input('Digite o primeiro valor que deseja multiplicar: '))
         multiplicaçao2 = float(input('Digite o segundo valor que deseja multiplicar: '))
         result4 = int(multiplicaçao1 * multiplicaçao2)
-        print(f'''{multiplicaçao1} * {multiplicaçao2} = {result4}
-===============================================================''')
+        print(f'{multiplicaçao1} * {multiplicaçao2} = {result4}')
+        print('+='*40)
+        
     if operaçao == 5:
         print('+='*40)
         print('OBRIGADO E ATÉ MAIS (: ')
